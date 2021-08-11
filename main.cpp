f8feaad13ae3f9e15d0d2c8189c6ff626415b7e1
VitalityEdge42 <doraemonlemons@gmail.com>
2021-08-12 07:42:46 +0800

    fucku

--- main.cpp
+++ main.cpp
@@ -1,10 +1,39 @@
 #include <iostream>
 #include <string>
+#include <array>
 
-class Chess{
-    public:
-        // Code
-        void GayGenerator(std::string &name);
+
+enum Pieces
+{
+    BlackPawn,
+    BlackKnight,
+    BlackBishop,
+    BlackRook,
+    BlackQueen,
+    BlackKing,
+
+    WhitePawn,
+    WhiteKnight,
+    WhiteBishop,
+    WhiteRook,
+    WhiteQueen,
+    WhiteKing,
+}
+
+
+class Board
+{
+public:
+    std::array<std::array<>, 8> board;
+
+    Board()
+    {
+
+    }
+}
+
+class Chess {
+
 };
 
 int main(){
